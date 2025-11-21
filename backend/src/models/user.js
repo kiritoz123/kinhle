617e0460ec5f81c90280cc4a1067291d675d8183
Huy <67195200+kiritoz123@users.noreply.github.com>
2025-11-21 13:56:55 +0700

    Add balance field to User model
    Add balance field to User model with default value.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -6,7 +6,8 @@ User.init({
   email: { type: DataTypes.STRING, allowNull: false, unique: true },
   name: { type: DataTypes.STRING },
   password: { type: DataTypes.STRING },
-  isAdmin: { type: DataTypes.BOOLEAN, defaultValue: false }
+  isAdmin: { type: DataTypes.BOOLEAN, defaultValue: false },
+  balance: { type: DataTypes.FLOAT, defaultValue: 0 } // Số dư tài khoản (VNĐ)
 }, {
   sequelize,
   modelName: 'User',
